71e465294cceeee7ac08f623114f8c28919ef3a5
Gordon Force Jr <gordonff@gmail.com>
2024-01-14 00:46:02 +0000

    Removed unused docker-compose files.

--- src/test/java/io/leftcoast/springboot3/Springboot3ApplicationTests.java
+++ src/test/java/io/leftcoast/springboot3/Springboot3ApplicationTests.java
@@ -1,13 +1,16 @@
 package io.leftcoast.springboot3;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot3ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {
+    final boolean passes = true;
+    assertThat(passes).isTrue();
+  }
 }

a726595f19b077d092beb50e493b35121b769d24
Gordon Force Jr <gordonff@gmail.com>
2024-01-15 02:24:18 +0000

    Get client and server generated plus, flesh out pom.xml file

--- generators/generate.sh
+++ generators/generate.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash -x
 
-GENERATED_SRC_BASE=gen
+###############################################################################
+# Remove previously generated code and create directories for generated files
+###############################################################################
 
-GENERATED_SRC_JAVA_BASE=${GENERATED_SRC_BASE}/client/src/java
+GEN_SRC_BASE=gen
 
-rm -rf $GENERATED_SRC_BASE
+rm -rf $GEN_SRC_BASE
 
-mkdir -p ${GENERATED_SRC_BASE}/client/src/{java,test}
+mkdir -p ${GEN_SRC_BASE}/{client,server}/src/{java,test}
+
+###############################################################################
+# Configuration common to all generator invocations
+###############################################################################
 
 PROJECT_NAME=springboot3webflux
 
@@ -21,15 +27,40 @@ COMMON_ARGS="--input-spec config/web-ping.yaml \
 
 PACKAGE_BASE=${GROUP_ID}.${PROJECT_NAME}
 
+######################################################################
+# Generate a Client using Java and Spring WebClient
+######################################################################
+
+GEN_CLIENT_SRC_JAVA_BASE=${GEN_SRC_BASE}/client/src/java
+
 CLIENT_PACKAGE_BASE=${PACKAGE_BASE}.client
 
 openapi-generator \
   generate \
-  --generator-name java \
-  ${COMMON_ARGS,//\'//,//([,=])\s+/$1/} \
-  --output ${GENERATED_SRC_JAVA_BASE}/client \
-  --artifact-id ${PROJECT_NAME}-client \
-  --api-package ${CLIENT_PACKAGE_BASE}.api \
-  --model-package ${CLIENT_PACKAGE_BASE}.model \
-  --invoker-package ${CLIENT_PACKAGE_BASE}.invoker \
-  --config bin/java-client-first.yaml
+    --generator-name java \
+    ${COMMON_ARGS,//\'//} \
+    --output ${GEN_CLIENT_SRC_JAVA_BASE}/server \
+    --artifact-id ${PROJECT_NAME}-server \
+    --api-package ${CLIENT_PACKAGE_BASE}.api \
+    --model-package ${CLIENT_PACKAGE_BASE}.model \
+    --invoker-package ${CLIENT_PACKAGE_BASE}.invoker \
+    --config generators/java-webclient.yaml
+
+######################################################################
+# Generate a Client using Java and Spring WebClient
+######################################################################
+
+GEN_SERVER_SRC_SPRING_BASE=${GEN_SRC_BASE}/server/src/java
+
+SERVER_PACKAGE_BASE=${PACKAGE_BASE}.server
+
+openapi-generator \
+  generate \
+    --generator-name spring \
+    ${COMMON_ARGS,//\'//} \
+    --output ${GEN_SERVER_SRC_SPRING_BASE}/server \
+    --artifact-id ${PROJECT_NAME}-server \
+    --api-package ${SERVER_PACKAGE_BASE}.api \
+    --model-package ${SERVER_PACKAGE_BASE}.model \
+    --invoker-package ${SERVER_PACKAGE_BASE}.invoker \
+    --config generators/spring-reactive.yaml
